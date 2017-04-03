7a8f9081d781916c2a554e2dd379139daf351293
Laura <laurabennett701@gmail.com>
2017-04-03 16:47:40 -0500

    first commit

---
+++ bubble_sort/bubble_sort.rb
@@ -0,0 +1,38 @@
+#A bubble sort sorts an array by comparing one value to the value next to it and swapping the two if the initial value is larger
+
+def bubble_sort(values)
+  compare = values.length
+  while compare > 0
+    count = 0
+    #iterate through every index of the array
+    while count < (compare-1)
+      if values[count] > values[count+1]
+        values[count], values[count+1] = values[count+1], values[count]
+      end #end of if statment
+      count += 1
+    end #end of inner while loop
+    compare -= 1
+  end #end of outer while loop
+  values
+end
+
+sorted_array = bubble_sort([4,3,78,2,0,2])
+
+
+def bubble_sort_by(values)
+  compare = values.length
+  while compare > 0
+    count = 0
+    #iterate through every index of the array
+    while count < (compare-1)
+      if yield(values[count],values[count+1]) > 0
+        values[count], values[count+1] = values[count+1], values[count]
+      end #end of if statment
+      count += 1
+    end #end of inner while loop
+    compare -= 1
+  end #end of outer while loop
+  values
+end
+
+sorted_values = bubble_sort_by(["hi","hello","hey"]) {|left,right| left.length - right.length}
